4a267ff2e334b7739220c84c4b4d8d5324a40f9b
ZOROo100 <karypbekov77@gmail.com>
2023-12-07 21:21:24 +0600

    homework1

--- month2/islam_37-3.hw1.py
+++
@@ -1,6 +0,0 @@
-class Home:
-    def new(self, value):
-        try:
-            int_value = int(value)
-        except ValueError:
-            raise ValueError("Аргумент должен быть целым числом.")
